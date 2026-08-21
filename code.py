7d858e874a9efd0014b7955513aff582d9bcedb9
khushboo-405 <041205.khushboo@gmail.com>
2026-08-21 23:27:54 +0530

    Delete code.py

--- code.py
+++
@@ -1,9 +0,0 @@
-first_name = "Tony"
-last_name = "Stark"
-age = 53
-height = 1.85
-super_hero = input("Enter the Super hero Name:")
-
-print("Name:",first_name+" "+last_name)
-print("height:",height)
-print("Superhero:",super_hero)

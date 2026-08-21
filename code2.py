31394073bf01a13281b7451be10479fa539928bf
khushboo-405 <041205.khushboo@gmail.com>
2026-08-21 19:19:49 +0530

    Add files via upload

---
+++ code2.py
@@ -0,0 +1,27 @@
+pencil = 99.5                                  
+pen = 23.75
+sharpner = 16.15
+#total bill amount
+total = pencil+pen+sharpner
+print("Total Bill Amount", total)
+
+#average price
+average = (pencil+pen+sharpner)/3
+print("average is", average)
+
+#finding the position of 'S' and 's' 
+name = "Superhero"
+print(name.find("S"))
+print(name.find("s"))
+
+#replacing 'S' with 's'
+print(name.replace("S","s"))
+
+#checking for the presence 
+print("s" in name)
+print("S" in name)
+
+#uppercase
+print(name.upper())
+#lowercase
+print(name.lower())
